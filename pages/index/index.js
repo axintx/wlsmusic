d135149ea210050ad3683f8b2983a9251b213d45
iit666 <121840415@qq.com>
2021-09-05 23:58:39 +0800

    feat(index): 排行榜

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,8 +4,9 @@ import request from '../../utils/request'
 Page({
   data: {
     bannerList: [], // 轮播图数据
-    recommendList: [] // 推荐歌单
-  },
+    recommendList: [], // 推荐歌单
+    topList: [] // 排行榜歌单
+   },
   // 事件处理函数
   bindViewTap() {
     wx.navigateTo({
@@ -15,19 +16,33 @@ Page({
   /**
    * 生命周期函数 -- 监听页面加载
    */
-  onLoad: async function(options) {
+  onLoad: async function (options) {
     let page = this
     // 获取轮播图数据
-    let bannerListData = await request('/banner', {type:2});
+    let bannerListData = await request('/banner', { type: 2 });
     this.setData({
       bannerList: bannerListData.banners
     })
 
-    // 获取推荐数据
+    // 获取推荐歌单
     // let recommendListData = await request('/personalized', {limit:20});
-    let recommendListData = await request('/personalized/newsong', {limit:10});
+    let recommendListData = await request('/personalized/newsong', { limit: 10 });
     this.setData({
       recommendList: recommendListData.result
     })
+
+    // 排行榜区域
+    let index = 0
+    let resultArr = [];
+    while (index < 5) {
+      let topListData = await request('/top/list', { idx: index++ })
+      console.log(topListData);
+      let topListItem = { name: topListData.playlist.name, tracks: topListData.playlist.tracks.slice(0, 3) };
+      resultArr.push(topListItem);
+      this.setData({
+        topList: resultArr
+      })
+    }
+
   }
 })
